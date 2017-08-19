d1575d5883c48319d7dd8ace580132f6aa4ea0b1
Shobi <ppshobi@gmail.com>
2017-08-19 21:58:39 +0530

    show incomplete tasks count

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -22,7 +22,11 @@ Template.body.helpers({
         });
     }
     return Tasks.find({}, { sort:{ createdAt: -1} });
-  }
+  },
+
+  incompleteCount() {
+      return Tasks.find({checked:{ $ne:true }}).count();
+  },
 });
 
 Template.body.events({
