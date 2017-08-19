6234bc4601ff798c4615f0c4549f9c8c79a85038
Shobi <ppshobi@gmail.com>
2017-08-19 21:53:04 +0530

    added reactDict + checkbox for showing only checked tasks

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -1,11 +1,26 @@
 import { Template } from 'meteor/templating';
+import { ReactiveDict } from 'meteor/reactive-dict';
+
 import { Tasks } from '../api/tasks.js';
  
 import './body.html';
 import './task.js';
  
+
+Template.body.onCreated( function bodyOncreadted() {
+    this.state = new ReactiveDict();
+});
+
 Template.body.helpers({
-  tasks(){
+  tasks() {
+    const instance = Template.instance();
+    if(instance.state.get('hideCompleted')){
+        return Tasks.find({ 
+              checked:{ $ne:true } 
+            }, 
+            { sort: { createdAt: -1} 
+        });
+    }
     return Tasks.find({}, { sort:{ createdAt: -1} });
   }
 });
@@ -23,5 +38,9 @@ Template.body.events({
         });
 
         target.text.value = '';
+    },
+
+    'change .hide-completed input' (event, instance) {
+        instance.state.set('hideCompleted', event.target.checked);
     }
 });
